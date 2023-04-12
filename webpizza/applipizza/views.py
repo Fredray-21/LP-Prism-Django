033db4210bc93ac5840f943e5b8184b0e1ceb952
Fredray-21 <dabadie.frederic@gmail.com>
2023-04-12 10:05:14 +0200

    feat(views) : généralisation des vues

--- webpizza/applipizza/views.py
+++ webpizza/applipizza/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from applipizza.models import Pizza
+from applipizza.models import Ingredient
 
 # Create your views here.
 def pizzas(request):
     lesPizzas = Pizza.objects.all()
     return render(request, 'applipizza/pizzas.html', {'pizzas': lesPizzas})
 
-
+def ingredients(request):
+    lesIngredients = Ingredient.objects.all()
+    return render(request, 'applipizza/ingredients.html', {'ingredients':lesIngredients})
