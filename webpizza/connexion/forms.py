3d1aafe5c142aad69dd225f3af4fe7648666aba9
Fredray-21 <dabadie.frederic@gmail.com>
2023-05-23 10:28:37 +0200

    fix(connexion) : fix erreur de redirection

--- webpizza/connexion/forms.py
+++ webpizza/connexion/forms.py
@@ -5,10 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(label='Prénom')
     last_name = forms.CharField(label='Nom')
-    email = forms.EmailField(label='Adresse e-mail');
+    email = forms.EmailField(label='Adresse e-mail')
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ('first_name','last_name','email')
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email')
 
 
--- webpizza/connexion/urls.py
+++ webpizza/connexion/urls.py
@@ -17,11 +17,11 @@ urlpatterns = [
              template_name='connexion/password_reset_confirm.html'),
              name='password_reset_confirm'),
     path('password_reset/done/',
-         auth_views.PasswordResetConfirmView.as_view(
+         auth_views.PasswordResetDoneView.as_view(
              template_name='connexion/password_reset_done.html'),
          name='password_reset_done'),
     path('reset/done/',
-         auth_views.PasswordResetConfirmView.as_view(
+         auth_views.PasswordResetCompleteView.as_view(
              template_name='connexion/password_reset_complete.html'),
          name='password_reset_complete'),
 ]
