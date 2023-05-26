2786152f9fb96b7055a765f1bd976c6e08e40642
Fredray-21 <dabadie.frederic@gmail.com>
2023-05-26 23:56:02 +0200

    feat(model) : ajout model oeuvre

--- ArtSpectra/appliArtSpectra/models.py
+++ ArtSpectra/appliArtSpectra/models.py
@@ -6,7 +6,16 @@ class Oeuvre(models.Model):
     idOeuvre = models.AutoField(primary_key=True)
 
     # nomOeuvre est un varchar de 50 caractères => CharField
-    nomOeuvre = models.CharField(max_length=50, verbose_name="Nom de l'oeuvre")
+    nomOeuvre = models.CharField(max_length=50, verbose_name="Nom de l'oeuvre", unique=True, default='')
+
+    # descriptionOeuvre est un varchar de 255 caractères => CharField
+    descriptionOeuvre = models.CharField(max_length=255, verbose_name="Description de l'oeuvre", blank=True,default='')
+
+    # dateCreationOeuvre est un date => DateField
+    dateCreationOeuvre = models.DateField(verbose_name="Date de création de l'oeuvre", default='1900-01-01')
+
+    # dimensionOeuvre est un varchar de 50 caractères => CharField
+    dimensionOeuvre = models.CharField(max_length=50, verbose_name="Dimension de l'oeuvre", blank=True,default='')
 
     # une methode de type "toString" pour afficher l'objet
     def __str__(self):
