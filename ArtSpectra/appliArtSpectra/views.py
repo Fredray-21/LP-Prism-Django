f32c79dcc478ad2ded9db9e61d8509f036a8b30f
Fredray-21 <dabadie.frederic@gmail.com>
2023-05-27 22:24:49 +0200

    feat(views) : ajout first view Oeuvres

--- ArtSpectra/appliArtSpectra/views.py
+++ ArtSpectra/appliArtSpectra/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from appliArtSpectra.models import Oeuvre
 # Create your views here.
+
+def accueil(request):
+    return render(request, 'appliArtSpectra/accueil.html')
+
+def oeuvres(request):
+    oeuvres = Oeuvre.objects.all()
+    return render(request, 'appliArtSpectra/oeuvres.html', {'oeuvres': oeuvres})
